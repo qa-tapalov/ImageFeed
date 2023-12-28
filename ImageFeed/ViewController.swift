d94236d970c07c4927504edee854ca9269e703ad
Андрей Тапалов <aatapalov@yandex.ru>
2023-12-28 10:43:45 +0400

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Тапалов on 28.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
