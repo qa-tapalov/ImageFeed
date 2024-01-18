2e3950efccb6313ca6c99369859d82b8b70be07d
Andrey Tapalov <aatapalov@yandex.ru>
2024-01-18 12:12:45 +0400

    Вёрстка экрана профиля, группировка ресурсов

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Тапалов on 18.01.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var userpickPhoto: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var userDescription: UILabel!
+    
+    //MARK: - lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func exitButton(_ sender: Any) {
+        
+    }
+}
