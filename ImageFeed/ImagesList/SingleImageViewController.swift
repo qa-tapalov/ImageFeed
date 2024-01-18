0b21b3f66b69703ea9769a844f3ca84f57652811
Andrey Tapalov <aatapalov@yandex.ru>
2024-01-18 19:48:27 +0400

    Создание UINavigationController и SingleImageViewController

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Тапалов on 18.01.2024.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
